ebf8cb933c8b6861fa913263c67163cc47b0c28c
SCDan0624 <dmayle1@hotmail.com>
2020-12-13 21:17:14 -0800

    Added loop for a tempsum variable

--- problem_solving_patterns/sliding_window/refactored_solution.js
+++ problem_solving_patterns/sliding_window/refactored_solution.js
@@ -7,6 +7,8 @@ function maxSubarraySum(arr, num) {
   for (let i = 0; i < arr.length; i++) {
     maxSum += arr[i];
   }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {}
 }
 
 // maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
