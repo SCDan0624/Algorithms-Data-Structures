901d91559dc1336bb18a55749debe96d0d7d1b96
SCDan0624 <dmayle1@hotmail.com>
2021-01-14 14:08:26 -0800

    Completed naive_solution

--- problem_solving_patterns/divide_and_conquer/naive_solution.js
+++ problem_solving_patterns/divide_and_conquer/naive_solution.js
@@ -9,3 +9,12 @@ search([1,2,3,4,5,6],4) // 3
 search([1,2,3,4,5,6],6) // 5
 search([1,2,3,4,5,6],11) // -1
 */
+
+function search(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
